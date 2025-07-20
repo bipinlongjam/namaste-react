1b9994e43cc0a281caa34c0a236c5b2f21c6134b
bipinlongjam <bipinlongjam11@gmail.com>
2025-07-20 22:38:55 +0530

    Igniting App

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
- const heading =  React.createElement("h1", {id:"heading"},"Hello World from React!");
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+ 
+
 
  const parent = React.createElement("div", {id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{}, "Hello From first child h1 tag"),
+    React.createElement("div",{id:"child", key:"child"},[
+        React.createElement("h1",{}, "This is Namaste React"),
         React.createElement("h2",{}, "Hello From first child h2 tag"),
     ]),
-     React.createElement("div",{id:"child2"},[
+     React.createElement("div",{id:"child2", key:"child2"},[
         React.createElement("h1",{}, "Hello From second child h1 tag"),
         React.createElement("h2",{}, "Hello From second child h2 tag"),
     ]) 
